21d0ab71069e34056b58e3f0cb78e3fcfcd88781
Ayush Suresh <byteslayer31037@gmail.com>
2025-02-15 19:57:16 -0600

    i think im getting things confused

--- programs/oracle-swap/src/lib.rs
+++ programs/oracle-swap/src/lib.rs
@@ -8,24 +8,26 @@ pub mod oracle_swap {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, data: InitializeArgs) -> Result<()> {
-        // 
-        ctx.swap_metadata.value = data
-        
+        ctx.accounts.swap_metadata= data
         Ok(())
+    }
+
+    pub fn swap(ctx: Context<Swap>, data: SwapArgs) -> Result<()> {
+        
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(mut)]
-    pub admin: Signer<'info>,
+    pub admin: Signer<'info>, // the user of the program (i.e. sending SOL)
 
     pub mint_incoming: InterfaceAccount<'info, Mint>,
 
     #[account(seeds = [b"incoming", mint_incoming.key().as_ref()], bump)]
-    pub token_account_incoming: InterfaceAccount<'info, TokenAccount>,
+    pub token_account_incoming: InterfaceAccount<'info, TokenAccount>, // program's token account
 
-    #[account(init, payer = admin, space = 8 + 100)]
+    #[account(init, payer = admin, space = 8 + 100, seeds=[b"metadata", mint_incoming.key().as_ref()], bump)]
     pub swap_metadata: InterfaceAccount<'info, SwapMetadata>,
 
     pub system_program: Program<'info, System>,
@@ -42,6 +44,29 @@ pub struct InitializeArgs {
 pub struct SwapMetadata {
     pub mint_incoming: Pubkey,
     pub discount_bps: u16,
+}
+
+// Defining context for swap function
+#[derive(Accounts)]
+pub struct Swap<'info> {
+    #[account(mut)]
+    pub admin: Signer<'info>, // the user of the program (i.e. sending SOL)
+
+    pub mint_incoming: InterfaceAccount<'info, Mint>,
+
+    #[account(seeds = [b"incoming", mint_incoming.key().as_ref()], bump)]
+    pub token_account_incoming: InterfaceAccount<'info, TokenAccount>, // program's token account
+
+    #[account(seeds = [b"metadata", mint_incoming.key().as_ref()], bump)]
+    pub swap_metadata: InterfaceAccount<'info, SwapMetadata>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Eq, PartialEq, Clone, Copy, Debug)]
+pub struct SwapArgs {
+    pub amount_in: u64, // the amount of SOL the user is sending in
+    pub oracle_price: u64, // amount of 2Z per SOL
 }
 
 
