1cfbefe137a949808e8fff1ad4ed7c58b70f0787
ani <anirudhtx@gmail.com>
2025-02-16 22:51:24 -0600

    fix bug

--- programs/oracle-swap/src/lib.rs
+++ programs/oracle-swap/src/lib.rs
@@ -19,17 +19,17 @@ pub mod oracle_swap {
 
         ctx.accounts.swap_metadata.admin = *ctx.accounts.admin.key;
         ctx.accounts.swap_metadata.mint_incoming = ctx.accounts.mint_incoming.key();
-        ctx.accounts.swap_metadata.pyth_feed_id = data.pyth_feed_id;
+        ctx.accounts.swap_metadata.feed_id_incoming = data.feed_id_incoming;
         ctx.accounts.swap_metadata.discount_bps = data.discount_bps;
         Ok(())
     }
 
     pub fn swap(ctx: Context<Swap>, data: SwapArgs) -> Result<()> {
-        let pyth_price = get_pyth_price_from_update(&mut ctx.accounts.price_update, ctx.accounts.swap_metadata.pyth_feed_id, MAXIMUM_AGE)?;
-        let price_sol = get_discounted_price(pyth_price, ctx.accounts.swap_metadata.discount_bps);
-        let price_incoming = 500u64;
+        let price_sol = get_pyth_price_from_update(&mut ctx.accounts.price_update_sol, SOL_FEED_ID, MAXIMUM_AGE)?;
+        let price_sol_discount = get_discounted_price(price_sol, ctx.accounts.swap_metadata.discount_bps);
+        let price_incoming = get_pyth_price_from_update(&mut ctx.accounts.price_update_incoming, ctx.accounts.swap_metadata.feed_id_incoming, MAXIMUM_AGE)?;
 
-        let sol_outgoing = data.amount_incoming * price_incoming / price_sol;
+        let sol_outgoing = data.amount_incoming * price_incoming / price_sol_discount;
 
         transfer_token_if_needed(
             &ctx.accounts.ta_swapper, 
@@ -79,7 +79,7 @@ pub struct Initialize<'info> {
 #[derive(AnchorSerialize, AnchorDeserialize, Eq, PartialEq, Clone, Copy, Debug)]
 pub struct InitializeArgs {
     pub discount_bps: u16,
-    pub pyth_feed_id: [u8; 32],
+    pub feed_id_incoming: [u8; 32],
 }
 
 #[derive(Accounts)]
@@ -107,7 +107,9 @@ pub struct Swap<'info> {
     #[account(constraint = mint_incoming.key() == swap_metadata.mint_incoming)]
     pub mint_incoming: InterfaceAccount<'info, Mint>,
 
-    pub price_update: Account<'info, PriceUpdateV2>,
+    pub price_update_sol: Account<'info, PriceUpdateV2>,
+
+    pub price_update_incoming: Account<'info, PriceUpdateV2>,
 
     #[account(seeds = [SEED_SWAP_METADATA], bump)]
     pub swap_metadata: Account<'info, SwapMetadata>,
--- programs/oracle-swap/src/state.rs
+++ programs/oracle-swap/src/state.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
 
 pub const MAXIMUM_AGE: u64 = 15;
+pub const SOL_FEED_ID: [u8; 32] = [239, 13, 139, 111, 218, 44, 235, 164, 29, 161, 93, 64, 149, 209, 218, 57, 42, 13, 47, 142, 208, 198, 199, 188, 15, 76, 250, 200, 194, 128, 181, 109];
 
 pub const RESERVE_SWAP_METADATA: usize = 8 + 32 + 32 + 32 + 2 + 100;
 pub const SEED_SWAP_METADATA: &[u8] = b"swap_metadata";
@@ -10,6 +11,6 @@ pub const SEED_SWAP_METADATA: &[u8] = b"swap_metadata";
 pub struct SwapMetadata {
     pub admin: Pubkey,
     pub mint_incoming: Pubkey,
-    pub pyth_feed_id: [u8; 32],
+    pub feed_id_incoming: [u8; 32],
     pub discount_bps: u16,
 }
